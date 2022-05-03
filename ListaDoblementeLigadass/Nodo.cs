d8042078db87cc67e1c8ab16d42fea7457c443c4
ElzarMachado <101478824+ElzarMachado@users.noreply.github.com>
2022-05-02 22:21:11 -0700

    Se agrego en clase nodo

--- ListaDoblementeLigadass/Nodo.cs
+++ ListaDoblementeLigadass/Nodo.cs
@@ -4,9 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ListaDoblementeLigadass
+namespace ListasDoblementeLigadass
 {
-    internal class Nodo
+    public class Nodo
     {
+        public string Valor { get; set; }
+        public Nodo Ant { get; set; }
+        public Nodo Sig { get; set; }
+        public Nodo(string valor = "", Nodo ant = null, Nodo sig = null)
+        {
+            Valor = valor;
+            Ant = ant;
+            Sig = sig;
+        }
     }
 }
