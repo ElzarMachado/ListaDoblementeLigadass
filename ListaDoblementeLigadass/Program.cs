8078bc42c7de6fdd16e89f938bec74996dc3c37d
ElzarMachado <101478824+ElzarMachado@users.noreply.github.com>
2022-05-02 22:56:19 -0700

    program.cs del trabajo pasado para poder modificarlo

--- ListaDoblementeLigadass/Program.cs
+++ ListaDoblementeLigadass/Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿using ListasSimplesLigadas;
+
+Listas listas = new Listas();
+listas.AgregarNodo("Cero");
+listas.AgregarNodo("Uno");
+listas.AgregarNodo("Dos");
+listas.AgregarNodo("Ocho");
+Console.WriteLine(listas.RecorrerLista());
+
+Console.WriteLine();
+Console.WriteLine("Buscando Nodo");
+Nodo nodoDos = listas.Buscar("Dos");
+Console.WriteLine(nodoDos.Valor);
+
+Console.WriteLine();
+
+Console.WriteLine("Se borro nodo");
+listas.BorrarNodo("Dos");
+Console.WriteLine(listas.RecorrerLista());
